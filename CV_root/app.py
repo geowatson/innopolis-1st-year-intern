099d1134a407aa6a6605cf9a397b1d95533e831c
indionapolis <panddromas@gmail.com>
2018-05-19 22:16:07 +0300

    sample system

--- CV_root/app.py
+++ CV_root/app.py
@@ -1,6 +1,131 @@
 import cv2
+import os
+import numpy as np
+import requests
 
-print(cv2.face)
+
+subjects = ["", "Shahrukh Khan", "Not Tom Cruise", "Tom Cruise", "Pavlo"]
+
+# function to detect face using OpenCV
+def detect_face(img):
+    # convert the test image to gray image as opencv face detector expects gray images
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # load OpenCV face detector, I am using LBP which is fast
+    # there is also a more accurate but slow Haar classifier
+    face_cascade = cv2.CascadeClassifier('opencv-files/lbpcascade_frontalface.xml')
+
+    # let's detect multiscale (some images may be closer to camera than others) images
+    # result is a list of faces
+    faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.2, minNeighbors=5);
+
+    # if no faces are detected then return original img
+    if (len(faces) == 0):
+        return None, None
+
+    # under the assumption that there will be only one face,
+    # extract the face area
+    (x, y, w, h) = faces[0]
+
+    # return only the face part of the image
+    return gray_img[y:y + w, x:x + h], faces[0]
 
 
+# this function will read all persons' training images, detect face from each image
+# and will return two lists of exactly same size, one list
+# of faces and another list of labels for each face
+def prepare_training_data(data_folder_path):
+    # ------STEP-1--------
+    # get the directories (one directory for each subject) in data folder
+    dirs = os.listdir(data_folder_path)
 
+    # list to hold all subject faces
+    faces = []
+    # list to hold labels for all subjects
+    labels = []
+
+    # let's go through each directory and read images within it
+    for dir_name in dirs:
+
+        # our subject directories start with letter 's' so
+        # ignore any non-relevant directories if any
+        if not dir_name.startswith("s"):
+            continue;
+
+        # ------STEP-2--------
+        # extract label number of subject from dir_name
+        # format of dir name = slabel
+        # , so removing letter 's' from dir_name will give us label
+        label = int(dir_name.replace("s", ""))
+
+        # build path of directory containin images for current subject subject
+        # sample subject_dir_path = "training-data/s1"
+        subject_dir_path = data_folder_path + "/" + dir_name
+
+        # get the images names that are inside the given subject directory
+        subject_images_names = os.listdir(subject_dir_path)
+
+        # ------STEP-3--------
+        # go through each image name, read image,
+        # detect face and add face to list of faces
+        for image_name in subject_images_names:
+
+            # ignore system files like .DS_Store
+            if image_name.startswith("."):
+                continue;
+
+            # build image path
+            # sample image path = training-data/s1/1.pgm
+            image_path = subject_dir_path + "/" + image_name
+
+            # read image
+            image = cv2.imread(image_path)
+
+            # detect face
+            face, rect = detect_face(image)
+
+            # ------STEP-4--------
+            # for the purpose of this tutorial
+            # we will ignore faces that are not detected
+            if face is not None:
+                # add face to list of faces
+                faces.append(face)
+                # add label for this face
+                labels.append(label)
+
+    return faces, labels
+
+
+faces, labels = prepare_training_data("training-data")
+
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+face_recognizer.train(faces, np.array(labels))
+
+# this function recognizes the person in image passed
+# and draws a rectangle around detected face with name of the
+# subject
+def predict(test_img):
+    # make a copy of the image as we don't want to chang original image
+    img = test_img.copy()
+    # detect face from the image
+    face, rect = detect_face(img)
+
+    # predict the image using our face recognizer
+    label = face_recognizer.predict(face)
+    # get name of respective label returned by face recognizer
+    label_text = subjects[label[0]]
+
+    return label_text + ':' + str(label[1])
+
+
+#load test images
+test_img1 = cv2.imread("test-data/test1.jpg")
+test_img2 = cv2.imread("test-data/test2.jpg")
+test_img3 = cv2.imread("test-data/test3.jpg")
+
+#perform a prediction
+predicted_img1 = predict(test_img1)
+predicted_img2 = predict(test_img2)
+predicted_img3 = predict(test_img3)
+print([predicted_img1,predicted_img2,predicted_img3])
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # innopolis-1st-year-intern
 dev
-v
+v1.1
