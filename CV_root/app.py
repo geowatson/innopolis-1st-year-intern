e981d7e205fefc8d82bee40ea5389c5784c0f4cc
indionapolis <panddromas@gmail.com>
2018-05-20 21:59:00 +0300

    v1.0

--- CV_root/app.py
+++ CV_root/app.py
@@ -1,131 +1,137 @@
 import cv2
 import os
 import numpy as np
-import requests
+import socket
+import time
 
+subjects = ["", "Shahrukh Khan", "Elvis", "Tom Cruise", "Pavlo"]
+casc_path = 'opencv-files/lbpcascade_frontalface.xml'
+face_cascade = cv2.CascadeClassifier(casc_path)
 
-subjects = ["", "Shahrukh Khan", "Not Tom Cruise", "Tom Cruise", "Pavlo"]
+print('start')
 
-# function to detect face using OpenCV
 def detect_face(img):
-    # convert the test image to gray image as opencv face detector expects gray images
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # load OpenCV face detector, I am using LBP which is fast
-    # there is also a more accurate but slow Haar classifier
-    face_cascade = cv2.CascadeClassifier('opencv-files/lbpcascade_frontalface.xml')
-
-    # let's detect multiscale (some images may be closer to camera than others) images
-    # result is a list of faces
     faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.2, minNeighbors=5);
 
-    # if no faces are detected then return original img
     if (len(faces) == 0):
         return None, None
 
-    # under the assumption that there will be only one face,
-    # extract the face area
     (x, y, w, h) = faces[0]
 
-    # return only the face part of the image
-    return gray_img[y:y + w, x:x + h], faces[0]
+    return cv2.resize(gray_img[y:y + w, x:x + h], (600, 500)), faces[0]
 
 
-# this function will read all persons' training images, detect face from each image
-# and will return two lists of exactly same size, one list
-# of faces and another list of labels for each face
-def prepare_training_data(data_folder_path):
-    # ------STEP-1--------
-    # get the directories (one directory for each subject) in data folder
+def prepare_training_data(data_folder_path="training-data"):
     dirs = os.listdir(data_folder_path)
 
-    # list to hold all subject faces
     faces = []
-    # list to hold labels for all subjects
     labels = []
 
-    # let's go through each directory and read images within it
     for dir_name in dirs:
 
-        # our subject directories start with letter 's' so
-        # ignore any non-relevant directories if any
         if not dir_name.startswith("s"):
             continue;
 
-        # ------STEP-2--------
-        # extract label number of subject from dir_name
-        # format of dir name = slabel
-        # , so removing letter 's' from dir_name will give us label
         label = int(dir_name.replace("s", ""))
 
-        # build path of directory containin images for current subject subject
-        # sample subject_dir_path = "training-data/s1"
         subject_dir_path = data_folder_path + "/" + dir_name
 
-        # get the images names that are inside the given subject directory
         subject_images_names = os.listdir(subject_dir_path)
 
-        # ------STEP-3--------
-        # go through each image name, read image,
-        # detect face and add face to list of faces
         for image_name in subject_images_names:
 
-            # ignore system files like .DS_Store
             if image_name.startswith("."):
                 continue;
 
-            # build image path
-            # sample image path = training-data/s1/1.pgm
             image_path = subject_dir_path + "/" + image_name
-
-            # read image
             image = cv2.imread(image_path)
 
-            # detect face
             face, rect = detect_face(image)
 
-            # ------STEP-4--------
-            # for the purpose of this tutorial
-            # we will ignore faces that are not detected
             if face is not None:
-                # add face to list of faces
                 faces.append(face)
-                # add label for this face
                 labels.append(label)
 
     return faces, labels
 
 
-faces, labels = prepare_training_data("training-data")
-
+faces, labels = prepare_training_data()
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-
 face_recognizer.train(faces, np.array(labels))
+video_capture = None
 
-# this function recognizes the person in image passed
-# and draws a rectangle around detected face with name of the
-# subject
-def predict(test_img):
-    # make a copy of the image as we don't want to chang original image
-    img = test_img.copy()
-    # detect face from the image
-    face, rect = detect_face(img)
+print('Training done')
 
-    # predict the image using our face recognizer
-    label = face_recognizer.predict(face)
-    # get name of respective label returned by face recognizer
-    label_text = subjects[label[0]]
-
-    return label_text + ':' + str(label[1])
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(("", 5000))
+server_socket.listen(5)
 
 
-#load test images
-test_img1 = cv2.imread("test-data/test1.jpg")
-test_img2 = cv2.imread("test-data/test2.jpg")
-test_img3 = cv2.imread("test-data/test3.jpg")
+# wait for ip camera url
+while True:
+    try:
+        client_socket, address = server_socket.accept()
+        print("Conencted to - ", address, "\n")
+        cam_url = client_socket.recv(1024).decode("utf-8")
+        print(cam_url)
+        video_capture = cv2.VideoCapture(cam_url)
+        break
 
-#perform a prediction
-predicted_img1 = predict(test_img1)
-predicted_img2 = predict(test_img2)
-predicted_img3 = predict(test_img3)
-print([predicted_img1,predicted_img2,predicted_img3])
+    except socket.timeout:
+        continue
+    #except Exception:
+        #server_socket.close()
+
+# watch ip camera stream
+while True:
+    try:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        if not frame:
+            pass
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # looking for faces in frame
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+
+            label = face_recognizer.predict(gray[y:y + w, x:x + h])
+            if label[1] > 30.0:
+                continue
+            print(subjects[label[0]])
+            print(label[1])
+            time.sleep(4)
+
+            #label_text = subjects[label[0]]
+            #cv2.putText(frame, label_text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+            #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Display the resulting frame
+        #frame = cv2.resize(frame, (600, 500))
+        #cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    except KeyError: #cv2.error:
+        continue
+    except ValueError:
+        print(video_capture.isOpened())
+        time.sleep(4)
+        continue
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
